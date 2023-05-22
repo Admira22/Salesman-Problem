9f0634ae97eed0d17ffee4e5e09fd5a9b4377957
Admira Bakal <72883607+Admira22@users.noreply.github.com>
2023-05-22 23:29:44 +0200

    landscape and portrait, translation

--- app/src/main/java/com/example/projekat/DetailScreen.kt
+++ app/src/main/java/com/example/projekat/DetailScreen.kt
@@ -1,10 +1,12 @@
 package com.example.projekat
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,28 +19,54 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun DetailScreen(){
     Column{
+        val configuration = LocalConfiguration.current
         val image = painterResource(id = R.drawable.image2)
-        Image(
-            painter = image,
-            contentDescription = null,
-        )
-        Text(
-            text = stringResource(R.string.title),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+        when (configuration.orientation){
+            Configuration.ORIENTATION_PORTRAIT ->{
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                )
+                Text(
+                    text = stringResource(R.string.title),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
 
-        )
-        Text(
-            text = stringResource(R.string.intro),
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
-        )
-        Text(
-            text = stringResource(R.string.text),
-            textAlign = TextAlign.Justify,
-            modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
-        )
+                )
+                Text(
+                    text = stringResource(R.string.intro),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.text),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+                )
+            }
+            else ->
+            {
+                Text(
+                    text = stringResource(R.string.title),
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+
+                )
+                Text(
+                    text = stringResource(R.string.intro),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+                )
+                Text(
+                    text = stringResource(R.string.text),
+                    textAlign = TextAlign.Justify,
+                    modifier = Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp)
+                )
+            }
+        }
+
     }
 }
 @Composable
